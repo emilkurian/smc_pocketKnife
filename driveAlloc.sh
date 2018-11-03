e96e24f53bb68fbc71929ee48c5fd38e92c88ecc
joshua <joshua@localhost.localdomain>
2018-11-02 17:33:32 -0700

    Friday Update

--- driveAlloc.sh
+++ driveAlloc.sh
@@ -1,1 +1,16 @@
-lsscsi -g | grep "disk" | grep "1:0" > disk.txt
+#!/bin/bash
+
+PWD=$(pwd)
+echo "$PWD"
+
+lsscsi -g > /home/joshua/pocketKnife/dump.txt 
+awk '{print $4"\t"$6"\t"$7"\t"$8}' /home/joshua/pocketKnife/dump.txt > /home/joshua/pocketKnife/data.txt
+column -t /home/joshua/pocketKnife/data.txt > /home/joshua/pocketKnife/drives.txt
+rm /home/joshua/pocketKnife/data.txt /home/joshua/pocketKnife/dump.txt
+
+
+
+
+
+
+
